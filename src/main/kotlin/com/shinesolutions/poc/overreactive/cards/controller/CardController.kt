ca5147b9045fab67d3fde4b921998130745339e0
Branislav Minic <minicb79@gmail.com>
2019-09-16 11:03:51 +1000

    Added NonReactive Card Service methods

--- src/main/kotlin/com/shinesolutions/poc/overreactive/cards/controller/CardController.kt
+++ src/main/kotlin/com/shinesolutions/poc/overreactive/cards/controller/CardController.kt
@@ -2,21 +2,18 @@ package com.shinesolutions.poc.overreactive.cards.controller
 
 import com.shinesolutions.poc.overreactive.cards.model.Card
 import com.shinesolutions.poc.overreactive.cards.service.CardService
+import com.shinesolutions.poc.overreactive.cards.service.NonReactiveCardService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @RestController
-class CardController(@Autowired val cardService: CardService) {
+class CardController(@Autowired val cardService: CardService,
+                     @Autowired val nrCardService: NonReactiveCardService) {
 
+    ///// REACTIVE ENDPOINTS
     @GetMapping("/cards")
     fun getCardList(): Flux<List<Card>> = cardService.findAll()
 
@@ -29,4 +26,21 @@ class CardController(@Autowired val cardService: CardService) {
 
     @PutMapping("/cards/{id}")
     fun updateCard(@PathVariable("id") id: Long, @RequestBody card: Card): Mono<Card> = cardService.update(id, card)
+
+    @DeleteMapping("/cards/{id}")
+    fun deleteCard(@PathVariable("id") id: Long) = cardService.delete(id);
+
+    ///// NON-REACTIVE ENDPOINTS
+    @GetMapping("/nr/cards")
+    fun getNonReactiveCardList(): List<Card> = nrCardService.findAll()
+
+    @GetMapping("/nr/cards/{id}")
+    fun getNonReactiveCard(@PathVariable("id") id: Long): Card = nrCardService.findOne(id)
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/nr/cards")
+    fun createNonReactiveCard(@RequestBody card: Card): Card = nrCardService.create(card)
+
+    @PutMapping("/nr/cards/{id}")
+    fun updateNonReactiveCard(@PathVariable("id") id: Long, @RequestBody card: Card): Card = nrCardService.update(id, card)
 }
